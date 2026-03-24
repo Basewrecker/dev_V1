c4b3bafd2c69f1a3a736d4586219fd88c449bdf0
Basewrecker <siddhunrs@gmail.com>
2026-03-24 22:36:09 +0530

    added pagination

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import ProjectCard from "~/components/ProjectCard";
@@ -11,16 +12,40 @@ export async function clientLoader({request}: Route.ClientLoaderArgs): Promise<{
 
 const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
   const {projects} = loaderData as {projects: Project[]};
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 2;
+
+  const totalPage = Math.ceil(projects.length / projectsPerPage);
+  const indexOfLast = currentPage * projectsPerPage;
+  const indexOfFirst = indexOfLast - projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+
+  const renderPagination = () => {
+    <div className="flex justify-center gap-2 mt-8">
+      {Array.from({length: totalPage, }, (_, index) => (
+        <button key = {index + 1} className={`px-3 py-1 cursor-pointer rounded ${
+          currentPage === index + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'
+        }`}
+        onClick={() => setCurrentPage(index + 1)}
+        >
+          {index + 1};
+        </button>
+      ))}
+    </div>
+  }
+
   return (
     <>
       <h2 className = "text-3xl text-white font-bold mb-8">
        Projects
       </h2>
       <div className="grid gap-6 sm:grid-cols-2">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
              <ProjectCard key = {project.id} project = {project}/>
           ))}
       </div>
+      {totalPage > 1 && renderPagination}
     </>
   )
 }

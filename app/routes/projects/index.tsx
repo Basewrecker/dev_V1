3951451935234092db36a2e71e62fbd1702707d4
Basewrecker <siddhunrs@gmail.com>
2026-03-25 15:38:06 +0530

    made pagination into a component

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import ProjectCard from "~/components/ProjectCard";
+import Pagination from "~/components/Pagination";
 
 export async function clientLoader({request}: Route.ClientLoaderArgs): Promise<{projects: Project[]}> {
   const res = await fetch('http://localhost:8000/projects')
@@ -21,23 +22,6 @@ const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
   const indexOfFirst = indexOfLast - projectsPerPage;
   const currentProjects = projects.slice(indexOfFirst, indexOfLast);
 
-  const renderPagination = () => {
-    return (
-      <div className="flex justify-center gap-2 mt-8">
-      {Array.from({length: totalPage, }, (_, index) => (
-        <button key = {index + 1} className={`px-3 py-1 cursor-pointer rounded ${
-          currentPage === index + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'
-        }`}
-        onClick={() => setCurrentPage(index + 1)}
-        >
-          {index + 1}
-        </button>
-      ))}
-    </div>
-    )
-    
-  }
-
   return (
     <>
       <h2 className = "text-3xl text-white font-bold mb-8">
@@ -48,7 +32,7 @@ const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
              <ProjectCard key = {project.id} project = {project}/>
           ))}
       </div>
-      {totalPage > 1 && renderPagination()}
+      <Pagination totalPages={totalPage} currentPage={currentPage} onPageChange={setCurrentPage}/>
     </>
   )
 }
